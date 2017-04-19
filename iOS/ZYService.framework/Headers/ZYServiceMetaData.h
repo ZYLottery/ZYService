0414891a056bcd0d9ada524a01226b52fd373344
guanxuhang1234 <18611817195@163.com>
2017-04-19 11:47:50 +0800

    0.0.5.5

---
+++ iOS/ZYService.framework/Headers/ZYServiceMetaData.h
@@ -0,0 +1,16 @@
+//
+//  ZYServiceMetaData.h
+//  ZYService
+//
+//  Created by guanxuhang1234 on 2017/4/19.
+//  Copyright © 2017年 章鱼彩票. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZYServiceMetaData : NSObject
+/**
+ 请求页面来源
+ */
+@property(nonatomic,copy)NSString * referrer;
+@end
